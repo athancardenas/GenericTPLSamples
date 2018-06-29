6886a47bb2ea980bef62f77370505feeabd17324
athancardenas <athancardenas@gmail.com>
2018-06-29 18:48:00 +0800

    Initial commit

---
+++ GenericTPLSamples/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GenericTPLSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IntObject> intObjects = new List<IntObject>() {
+                new IntObject() { IntProperty = 1 },
+                new IntObject() { IntProperty = 2 },
+                new IntObject() { IntProperty = 3 },
+            };
+
+            TplThis(IncrementPrint, intObjects);
+        }
+
+        static void TplThis<T>(Action<T> method, List<T> arg)
+        {
+            List<Task> tasks = new List<Task>();
+
+            arg.ForEach(a => {
+                tasks.Add( Task.Factory.StartNew( () => method(a) ) );
+            });
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        static void IncrementPrint(IntObject intObject)
+        {
+            Console.WriteLine($"{++intObject.IntProperty}");
+        }
+        
+    }
+
+    public class IntObject
+    {
+        public int IntProperty { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# GenericTPLSamples
